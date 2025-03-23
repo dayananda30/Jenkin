8239ce8bc534e98ac92b7266e8227c9b97bfb42a
Dayananda D R <Dayananda.DR@netapp.com>
2025-03-23 08:15:51 +0530

    Adding a small application

---
+++ simple-maven-app/src/main/java/com/example/Calculator.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Calculator {
+
+    /**
+     * Returns the sum of two numbers.
+     *
+     * @param a first operand
+     * @param b second operand
+     * @return the sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
